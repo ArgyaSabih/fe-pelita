8bf4eaaf566d26d9af4ecd6c0c23e44c684de6ff
ArgyaSabih <argy.sabih@gmail.com>
2025-11-11 17:45:48 +0700

    feat: navbar

--- app/layout.js
+++ app/layout.js
@@ -1,27 +1,63 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/globals.css";
 import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/navbar/Navbar";
+import { cn } from "@/utils/cn";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { AdlamDisplayRegular, FarroBold, FarroRegular, FarroMedium } from "@/utils/font";
 
 export const metadata = {
   title: "PELITA",
   description: "Pendamping Lika-Liku TK Anak",
+
+  icons: {
+    icon: [
+      {
+        url: `/assets/favicon/android-chrome-192x192.png`,
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: `/assets/favicon/android-chrome-512x512.png`,
+        sizes: "512x512",
+        type: "image/png",
+      },
+      { url: `/assets/favicon/favicon.ico` },
+    ],
+    apple: [
+      {
+        url: `/assets/favicon/apple-touch-icon.png`,
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+    shortcut: [{ url: `/assets/favicon/favicon.ico` }],
+    other: [
+      {
+        rel: "android-chrome-192x192",
+        url: `/assets/favicon/android-chrome-192x192.png`,
+        sizes: "192x192",
+      },
+      {
+        rel: "android-chrome-512x512",
+        url: `/assets/favicon/android-chrome-512x512.png`,
+        sizes: "512x512",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body
+        className={cn(
+          AdlamDisplayRegular.variable,
+          FarroBold.variable,
+          FarroMedium.variable,
+          FarroRegular.variable,
+          "antialiased",
+        )}
+      >
         <Navbar />
         {children}
         <Footer />
--- src/components/Navbar.js
+++
@@ -1,7 +0,0 @@
-export default function Navbar() {
-  return (
-    <div className="fixed top-0 z-[999] h-full w-full">
-      <h1 className="text-center">Ini Navbar</h1>
-    </div>
-  );
-}
